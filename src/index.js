e5cc89c004351c8e54bca3e8efe9729ebac1335e
howdyfrom2019 <tjd1emd@naver.com>
2022-10-11 21:10:35 +0900

    counter with Redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
+import { legacy_createStore } from "redux";
+
 const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+/**
+ * @reducer: data를 수정하기 위한 목적의 함수.
+ * @store.dispatch: action을 줄 수 있음.
+ */
 
-number.innerText = count;
+const countModifier = (count = 0, action) => {
+    if (action.type === "minus") {
+        return --count;
+    } else if (action.type === "plus") {
+        return ++count;
+    }
+    return count;
+};
 
-const updateText = () => {
-    number.innerText = count;
+const countStore = legacy_createStore(countModifier);
+
+const onChange = () => {
+    number.innerText = countStore.getState();
 }
 
-const handleAdd = () => {
-    count++;
-    updateText();
-}
+countStore.subscribe(onChange);
+plus.addEventListener("click", () => countStore.dispatch({ type: "plus" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "minus" }));
+// let count = 0;
+// number.innerText = count;
 
-const handleMinus = () => {
-    count--;
-    updateText();
-}
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// const updateText = () => {
+//     number.innerText = count;
+// }
+
+// const handleAdd = () => {
+//     count++;
+//     updateText();
+// }
+//
+// const handleMinus = () => {
+//     count--;
+//     updateText();
+// }
+// plus.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
